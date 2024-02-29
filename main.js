6f72eb609197ea956b5c175461138cae8b5f6e67
shahjarar <160387866+shahjarar@users.noreply.github.com>
2024-02-29 17:38:55 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,2 @@
+var tranportationModes = ["sport Bike", "Bus", "Car", "sport Car"];
+tranportationModes.forEach(function (mode) { return console.log("I would like to own a ".concat(mode)); });
---
+++ main.ts
@@ -0,0 +1,3 @@
+let tranportationModes = ["sport Bike","Bus","Car","sport Car"];
+
+ tranportationModes.forEach(mode =>console.log(`I would like to own a ${mode}`));
